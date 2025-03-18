41a1b784222d7731d3c828758195340cd676f014
Dawid Bulinski <dawid.bulinski@xebia.com>
2025-03-18 21:04:02 +0100

    playwright - without exclusions

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "playwright/test";
+import { test } from "@chromatic-com/playwright";
 
 test("can visit contact page", async ({ page }) => {
   await page.goto("http://localhost:3000/contact");
